eb3be4edfba04291aaf3eb1bcd4d26a138d6e307
Rathish M <rathismr586@gmail.com>
2026-06-02 09:22:10 +0530

    Add files via upload

---
+++ bankclass.java
@@ -0,0 +1,12 @@
+class bankclass
+{
+String name;
+int acno;
+double balance;
+void display()
+{
+System.out.println("Account holder :"+name);
+System.out.println("Account no :"+acno);
+System.out.println("Bank balance :"+balance);
+}
+}
---
+++ bankobj.java
@@ -0,0 +1,11 @@
+class bankobj
+{
+public static void main(String[] args)
+{
+bankclass b1 = new bankclass();
+b1.name="Rathish";
+b1.acno=100805;
+b1.balance=960000;
+b1.display();
+}
+}
